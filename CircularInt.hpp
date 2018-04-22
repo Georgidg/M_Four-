7cd1b5b205bf0d8a4445d0eb1a3c6d19d6882d0d
GeorgiDg <33101215+Georgidg@users.noreply.github.com>
2018-04-22 10:20:42 +0300

    Add files via upload

---
+++ CircularInt.hpp
@@ -0,0 +1,50 @@
+#pragma once
+#include<iostream>
+#include<string>
+using namespace std;
+
+
+
+class CircularInt
+{
+
+public:
+	int start, end;
+    int current;
+	CircularInt(int, int);
+	CircularInt( const CircularInt& );
+	~CircularInt();
+
+	CircularInt& operator+=(int);
+	CircularInt &operator*=(const int);
+	CircularInt&  operator++();
+	CircularInt  operator++(int);
+
+	CircularInt operator+( int);
+	CircularInt operator+(const CircularInt&);
+	CircularInt operator--();
+//CircularInt& operator-=(int);
+	friend CircularInt operator/(const CircularInt&, const int );
+	friend CircularInt operator- (const CircularInt& other);
+	friend  CircularInt operator-(const int, const CircularInt&);
+
+	CircularInt operator=(int);
+	CircularInt operator/=(const int);
+	friend ostream& operator<<(ostream&, const CircularInt);
+
+};
+inline CircularInt operator- (const int num, const CircularInt& other )
+{
+	CircularInt temp(other);
+	 temp.current=num -temp.current  ;
+	 while(temp.current<temp.start)
+		 temp.current+=12;
+	return temp;
+}
+inline CircularInt operator- (const CircularInt& other)
+{
+	CircularInt temp(other);
+	temp.current = (temp.end - temp.current);
+	return temp;
+
+}
